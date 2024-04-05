ef48b294c76dbd2a698ed3b3946a9a4240efa1b8
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-04-05 21:18:11 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("Learn Go language by hello world")
+	
+	var name string="Prince";
+	var version=76;
+	fmt.Println(name);
+	fmt.Println(version);
+
+	var money int=67000
+	var currency =3498
+	fmt.Println(money)
+	fmt.Println("currency: ",currency)
+
+	var dimension float64=87.12
+	fmt.Println(dimension)
+
+	var decided bool=false
+	decided=true
+	fmt.Println(decided)
+
+	var person=23
+	fmt.Println(person)
+
+	const pi=76.12
+	fmt.Println(pi)
+
+	text :="Mohak Singh"
+	fmt.Println(text)
+
+	var Public ="data is important"
+	var private="data is private"
+
+	fmt.Println(Public)
+	fmt.Println(private)
+}
---
+++ myutil/myutil.go
@@ -0,0 +1,7 @@
+package myutil
+
+import "fmt"
+
+func PrintMessage(message string){
+	fmt.Printf(message)
+}
