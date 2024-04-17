306b59c96d309ee6ed920d623e7220b2c24acecb
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-04-17 21:29:55 +0530

    pointers done

---
+++ pointer/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func modifyValueByReference(num *int){
+	*num = *num * 20
+}
+
+func main() {
+	// var num int
+	num := "Mohak"
+
+	// var ptr *int
+	// ptr = &num
+
+	ptr:= &num
+
+	// fmt.Println("Num has value",num)
+	fmt.Println("Pointer contains: ",ptr)
+	fmt.Println("Data contains through pointer:", *ptr)
+
+	var pointer *int //default pointer ==nil
+	if  pointer ==nil {
+		fmt.Println("Pointer is not assigned")
+	}
+
+	value:=10
+	modifyValueByReference(&value)
+	fmt.Println("Value contains:",value)
+
+}
