2dbc2f66102c4997278f5032afaaa78b9adce54a
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-04-22 00:06:38 +0530

    date done

--- time/main.go
+++ time/main.go
@@ -12,4 +12,15 @@ func main() {
 
 	formatted := currentTime.Format("2006/01/02,03:04 PM")
 	fmt.Println("Formatted time is:",formatted)
-}
+
+	layout_str := "2006-01-02"
+	datStr := "2023-11-25"
+	formatted_time,_ :=time.Parse(layout_str,datStr)
+	fmt.Println("Formatted time is:",formatted_time)
+
+	//add 1 more day in currentTime
+	new_date := currentTime.Add(24*time.Hour)
+	fmt.Println("New date is:",new_date)
+	formatted_new_date:=new_date.Format("2006/01/02 Monday")
+	fmt.Println("Formatted new date is:",formatted_new_date)
+}	
