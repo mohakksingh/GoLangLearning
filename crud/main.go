205dd17bcc9752f19e6eb777f0b7edecaf92ca4e
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-04-28 20:37:35 +0530

    learnt about put method

--- crud/main.go
+++ crud/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"	
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
 )
@@ -83,9 +84,54 @@ func performPostRequest(){
 	fmt.Println("Response status: ",res.Status)
 }
 
+func performUpdateRequest(){
+	todo := Todo{
+		UserId: 2345,
+		Title: "Mohak Singh Go",
+		Completed: false,
+	}
+
+	//convert the Todo struct to JSON
+	jsonData,err := json.Marshal(todo)
+	if err!=nil{
+		fmt.Println("Error marshalling",err)
+		return
+	}
+
+	//convert json to string
+	jsonString := string(jsonData)
+
+	//convert string data to reader
+	jsonReader := strings.NewReader(jsonString)
+
+	const myUrl="https://jsonplaceholder.typicode.com/todos/1"
+
+	//create put request
+	req,err := http.NewRequest(http.MethodPut,myUrl,jsonReader)
+	if err!=nil{
+		fmt.Println("Error creating request",err)
+		return
+	}
+	req.Header.Set("Content-Type","application/json")
+
+	//send the request
+	client := http.Client{}
+	res,err := client.Do(req)
+	if err !=nil{
+		fmt.Println("Error sending request",err)
+		return
+	}
+	defer res.Body.Close()
+
+	data, _ := ioutil.ReadAll(res.Body)
+	fmt.Println("Response: ",string(data))
+	fmt.Println("Status is : ",res.Status)
+}
+
 func main() {
 	fmt.Println("Learning CRUD operations in GoLang")
-	//perfomGetRequest()
-	performPostRequest()
+	// perfomGetRequest()
+	// performPostRequest()
+	performUpdateRequest()
 
 }
