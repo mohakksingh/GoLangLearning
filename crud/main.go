fa54c02a283ad2de716427f4ffafda3939021fd7
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-04-29 22:13:59 +0530

    how we delete in go

--- crud/main.go
+++ crud/main.go
@@ -128,10 +128,34 @@ func performUpdateRequest(){
 	fmt.Println("Status is : ",res.Status)
 }
 
+func performDeleteRequest(){
+	const myUrl="https://jsonplaceholder.typicode.com/todos/1"
+
+	//create HTTP Req
+	req,err := http.NewRequest(http.MethodDelete,myUrl,nil)
+	if err!=nil{
+		fmt.Println("Error creating request",err)
+		return
+	}
+
+
+	//send the request
+	client := http.Client{}
+	res,err := client.Do(req)
+	if err!=nil{
+		fmt.Println("Error sending request",err)
+		return
+	}
+	defer res.Body.Close()
+
+	fmt.Println("Status is : ",res.Status)
+
+}
+
 func main() {
 	fmt.Println("Learning CRUD operations in GoLang")
 	// perfomGetRequest()
 	// performPostRequest()
-	performUpdateRequest()
-
+	// performUpdateRequest()
+	performDeleteRequest()
 }
